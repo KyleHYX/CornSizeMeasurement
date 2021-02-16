aaf9d6d5db6dcb0e6277a88fb82987b1ada34eeb
KyleHYX <60859865+KyleHYX@users.noreply.github.com>
2021-02-15 21:05:25 -0500

    add home and historical data fragment
    added home fragement and historical data fragment. Able to switch between fragments with bottom nav

---
+++ app/src/main/java/com/example/cornsizemeasurement/ui/historicalData/HistoricalData.kt
@@ -0,0 +1,33 @@
+package com.example.cornsizemeasurement.ui.historicalData
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cornsizemeasurement.R
+
+class HistoricalData : Fragment() {
+
+    companion object {
+        fun newInstance() = HistoricalData()
+    }
+
+    private lateinit var viewModel: HistoricalDataViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.historical_data_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HistoricalDataViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
